0dc35a0d888004176d0b81e1093cdd84bf5c9ead
Charles Smith <dreamserve@gmail.com>
2022-07-23 17:02:10 +0000

    navbar design completed

---
+++ src/components/navbar.jsx
@@ -0,0 +1,81 @@
+import { Badge } from "@material-ui/core";
+import { Search, ShoppingCartOutlined } from "@material-ui/icons";
+import React from "react";
+import styled from "styled-components";
+
+const Navbar = () => {
+  const Container = styled.div`
+    height: 60px;
+  `;
+  const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 20px;
+  `;
+  const Left = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+  `;
+  const Center = styled.div`
+    flex: 1;
+    text-align: center;
+  `;
+  const Right = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  `;
+  const Language = styled.div`
+    font-size: 14px;
+    cursor: pointer;
+    margin-left: 25px;
+  `;
+  const SearchContainer = styled.div`
+    border: 0.5px solid lightgray;
+    display: flex;
+    align-items: center;
+    margin-left: 25px;
+    padding: 5px;
+  `;
+  const Input = styled.input`
+    border: none;
+  `;
+  const Logo = styled.h1`
+    font-weight: bold;
+  `;
+  const MenuItem = styled.div`
+    font-size: 14px;
+    cursor: pointer;
+    margin-left: 25px;
+  `;
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input placeholder="search" />
+            <Search style={{ color: "gray", fontSize: "16px" }} />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo> Shoppy.</Logo>
+        </Center>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlined />
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Annoucement from "../components/annoucement";
+import Navbar from "../components/navbar";
+
+const Home = () => {
+  return (
+    <div>
+      <Annoucement />
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
