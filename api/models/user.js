8cd07a798aa6aa422fc9ef41c8bcc5b2707b65cf
Haseeb Asif <36411814+HaseebAsif@users.noreply.github.com>
2022-08-16 19:58:26 +0500

    Changes

--- api/models/user.js
+++ api/models/user.js
@@ -1,17 +1,17 @@
-const Mongoose = require("mongoose ");
+const mongoose = require("mongoose");
 
 const UserSchema = new mongoose.Schema(
   {
-    username: { type: string, required: true, unique: true },
-    email: { type: string, required: true, unique: true },
-    password: { type: string, required: true },
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
     isAdmin: {
       type: Boolean,
       default: false,
     },
-    img: { type: string },
+    img: { type: String },
   },
-
   { timestamps: true }
 );
-module.exports = Mongoose.model("User", UserSchema);
+
+module.exports = mongoose.model("User", UserSchema);
